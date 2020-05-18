258e05f74e9a6640f606f1ab812aae4fdfd67f3e
Lee <j005580@naver.com>
2020-05-19 01:51:15 +0900

    2020/05/19

---
+++ 2020/5/기능 개발.py
@@ -0,0 +1,16 @@
+def solution(progresses, speeds):
+    answer = [] 
+    while len(progresses)>0:
+        count = 0
+        for i in range(len(progresses)):
+            progresses[i] += speeds[i]
+        while len(progresses)>0:
+            if progresses[0]>=100:
+                progresses.pop(0)
+                speeds.pop(0)
+                count+=1
+            else:
+                break;   
+        if count !=0:
+            answer.append(count)
+    return answer
