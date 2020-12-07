d6094b2ba02cdf89435222b317a418d6aef47761
이준혁 <j005580@naver.com>
2020-12-07 19:31:12 +0900

    섬 연결하기(union find)

--- 2020/12/섬 연결하기.java
+++ 2020/12/섬 연결하기.java
@@ -1,71 +1,33 @@
 import java.util.*;
 
 class Solution {
-    Stack<Integer> skip = new Stack<>();
     public int solution(int n, int[][] costs) {
-        Arrays.sort(costs, (i1, i2) -> i2[2] - i1[2]);
         int answer = 0;
-        int start = 0;
-        while(start < costs.length){
-            skip.push(start);
-            boolean complete = false;
-            for(int i=0; i< n; i++){
-                if(isComplete(mkgraph(costs), n, i)){
-                    complete = true;
-                    break;
-                };
-            }
-            if(complete == false){
-                skip.pop();
-            }
-            start++;
+        int[] island = new int[n];
+        for(int i=0; i < n; i++){
+            island[i] = i;
         }
-        for(int i=0; i < costs.length; i++){
-            if(skip.contains(i)){
-                continue;
+        Arrays.sort(costs, (i1, i2) -> i1[2] - i2[2]);
+        for(int i=0; i< costs.length; i++){
+            if(find(island, costs[i][0]) != find(island, costs[i][1])){
+                merge(island, costs[i][0], costs[i][1]);
+                answer += costs[i][2];
             }
-            answer+=costs[i][2];
         }
+        
         return answer;
     }
     
-    public boolean isComplete(Map<Integer, List<Integer>> map, int n, int start){
-        if(!map.containsKey(start)){
-            return false;
+    public int find(int[] island, int x){
+        if(island[x] == x){
+            return x;
         }
-        Stack<Integer> toGo = new Stack<Integer>();
-        List<Integer> visited = new ArrayList<>();
-        toGo.push(start);
-        while(!toGo.isEmpty()){
-            int node = toGo.pop();
-            if(!visited.contains(node)){
-                visited.add(node);
-                if(map.containsKey(node)){
-                    toGo.addAll(map.get(node));
-                }
-                map.remove(node);
-            }
-        }
-        return visited.size() == n;
+        return find(island, island[x]);
     }
     
-    public Map<Integer, List<Integer>> mkgraph(int [][] costs){
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        for(int i=0; i < costs.length; i++){
-            if(skip.contains(i)){
-                continue;
-            }
-            if(!map.containsKey(costs[i][0])){
-                map.put(costs[i][0], new ArrayList<Integer>());
-            }
-            if(!map.containsKey(costs[i][1])){
-                map.put(costs[i][1], new ArrayList<Integer>());
-            }
-            map.get(costs[i][0]).add(costs[i][1]);
-            map.get(costs[i][1]).add(costs[i][0]);
-        }
-        return map;
+    public void merge(int[] island, int x, int y){
+        int parentX = find(island, x);
+        int parentY = find(island, y);
+        island[parentX] = parentY;
     }
-    
-  
 }
